60574634e7eb757cb89265b09328e62c9bcd3661
nekocan <panpacifickan2000@gmail.com>
2021-05-07 14:58:29 +0900

    send

--- p5.js
+++ p5.js
@@ -112,6 +112,7 @@ function draw() {
       Send(MOVING, new Vec(localVideo.pos.x / windowWidth , localVideo.pos.y / windowHeight));
     }
     if(localVideo.resulets || (localVideo.resulets === undefined && handResults)){
+      console.log('send');
       Send(HNDRES,localVideo.results);
       handResults = localVideo.resulets;
     }
