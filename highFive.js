ff2036ce4a73510a9705283ba114c59688c84473
NekoCan2000 <panpacifickan2000@gmail.com>
2021-09-30 23:34:25 +0900

    retake hikisu

--- highFive.js
+++ highFive.js
@@ -16,7 +16,7 @@ function HighFive() {
   for (let i = 0; i < 2; i++) {
     if (localHandsMinMax[i]) {
       DrawRectC(localVideo, localHandsMinMax[i], 1,color(0,0,255));
-      DrawCenterMarkC(localVideo, aveOthersHands[i], 2,color(0,0,255));
+      DrawCenterMarkC(localVideo, localHandsMinMax[i], 2,color(0,0,255));
     }
     if (aveOthersHands[i]) {
       DrawRectC(localVideo, aveOthersHands[i], 1,color(0,0,255));
