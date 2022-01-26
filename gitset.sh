2baf1f64f4de0f472b6af6472a002cb72982b17b
NekoCan2000 <panpacifickan2000@gmail.com>
2022-01-26 14:38:50 +0900

    バッシュにコメント「 」区切りで複数入れられるようにした はず

--- gitset.sh
+++ gitset.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 #引数の数チェック
-if [ $# < 1 ]; then
+if [ $# -lt 1 ]; then #引数が1未満(less than)
 	echo "コメントをつけてください" 1>&2
 	exit 1
 fi
 
+comments=""
+for comment; do
+	comments+="$comment "
+	echo "$comments"
+done
+
+max=${#comments}
+max=$((max-1))
+comments=${comments:0:$max}
+echo "$comments"
+
 #Git処理
 git add *
-git commit -m "$1"
+git commit -m "$comments"
 
 git push
