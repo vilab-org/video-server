d8af1410b66df0095c4f72cc56c37f9f7727fcac
NekoCan2000 <60868355NekoCan2000@users.noreply.github.com>
2022-04-23 17:02:18 +0900

    キャッチボールの整理

--- catchBall.js
+++ catchBall.js
@@ -38,7 +38,7 @@ function receiveBallStatus(fromAndTarget) {
       return;
     case CATCH:
       ballManager.setMode(TRACKING);
-      if(ballManager.host){
+      if (ballManager.host) {
         ballManager.selectTarget();
       }
       return;
@@ -58,21 +58,26 @@ function receiveBallStatus(fromAndTarget) {
         Send(CATCHBALL, THROWING);
         ballManager.setMode(THROWING);
       }
+    }, () => {
+      this.mode = TRACKING;
+      Send(CATCHBALL, CATCH);
     });
     ballManager.setTarget(target);
   }
 }
 
 class Ball extends Obj {
-  constructor(pos, target, throwFunc = () => { }) {
+  constructor(pos, target, throwFunc = () => { }, throwedFunc = () => { }) {
     super(pos, 50);
     this.target = target;//目標
     this.throwFunc = throwFunc;
+    this.throwedFunc = throwedFunc;
     this.mode = TRACKING;//ボールが動くモード
     this.from = target;//出発
     this.fromPos = createVector();
     this.rotate = 0;
     this.amt = 0;//線形補間の割合
+    this.speedR = 2;
   }
   update() {
     let from = this.from;
@@ -99,28 +104,27 @@ class Ball extends Obj {
           }
         }
         if (handsPos) {
+          let leftUp = getLeftUpPos(from);
+          this.pos.x = leftUp.x + handsPos.x * from.size.x;
+          this.pos.y = leftUp.y + handsPos.y * from.size.y;
           this.fromPos.x = this.pos.x;
           this.fromPos.y = this.pos.y;
-          if (handsPos.y < 0.3) {//投げた判定
+          if (getThrowJudge()) {//投げた判定
             this.throwFunc();
-          } else {
-            let leftUp = getLeftUpPos(from);
-            this.pos.x = leftUp.x + handsPos.x * from.size.x;
-            this.pos.y = leftUp.y + handsPos.y * from.size.y;
           }
+        }
+        function getThrowJudge() {
+          return handsPos && handsPos.y < 0.3;
         }
         break;
       case THROWING:
-        this.rotate += (1 / getFrameRate());
+        this.rotate += (1 / getFrameRate()) * this.speedR;
         this.pos = this.move();
-        this.amt += 1 / getFrameRate() / 3;//3秒で到達
+        this.amt += (1 / getFrameRate()) / 3;//3秒で到達
         if (this.amt >= 1) {
+          this.amt = 1;
           if (this.target.ID === localVideo.ID) {
-            this.mode = TRACKING;
-            Send(CATCHBALL, CATCH);
-            if(ballManager.host){
-              ballManager.finish();
-            }
+            throwedFunc();
           }
         }
         break;
@@ -152,6 +156,10 @@ class BallManager {
     this.ball = new Ball(localVideo.pos.copy(), localVideo, () => {
       Send(CATCHBALL, THROWING);
       this.setMode(THROWING);
+    }, () => {
+      this.mode = TRACKING;
+      Send(CATCHBALL, CATCH);
+      ballManager.finish();
     });
     this.selectTarget();
     this.host = true;
@@ -162,18 +170,23 @@ class BallManager {
   }
   //次の目標人物設定
   selectTarget() {
-    let next;
-    if (this.member.length > 0) {
-      let index = randomInt(this.member.length);
-      next = this.member[index];
-      this.member.splice(index, 1);
-    } else if (this.ball.target !== localVideo) {//もう渡ってない人がいない
-      next = localVideo;//ラスト自分
-    }
+    let next = getNext();
     this.setTarget(next);
     let msg = { from: this.ball.from.ID, target: this.ball.target.ID };
     if (log) console.log(msg);
     Send(CATCHBALL, msg);
+
+    function getNext() {
+      let next;
+      if (this.member.length > 0) {
+        let index = randomInt(this.member.length);
+        next = this.member[index];
+        this.member.splice(index, 1);
+      } else if (this.ball.target !== localVideo) {//もう渡ってない人がいない
+        next = localVideo;//ラスト自分
+      }
+      return next;
+    }
   }
   setTarget(next) {
     this.ball.setTarget(next);
