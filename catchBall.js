df4939472ce3cdfdf538950af08b7f5bca2896af
NekoCan2000 <60868355NekoCan2000@users.noreply.github.com>
2022-04-28 19:55:38 +0900

    Revert "Revert "targetがundefinedで送信するときはIDを送信しない件""
    This reverts commit 63a907d98d9486b7df568836a84cccaab203f204.

--- catchBall.js
+++ catchBall.js
@@ -34,21 +34,16 @@ function catchBallUpdate() {
     case BALLMODE_TRACKING:
       if (manager.selectMode === catchUserTypes[1]) {
         let lineP = getPointingLine(from);
-        if (lineP) {//指さしあり
+        if (ballManager.isUserHost && lineP) {//指さしあり
           let hitVideo = getCollVideo(from, lineP);
           push();
           stroke(0, 255, 0);
           strokeWeight(3);
-          if (hitVideo) {
-            line(lineP.start.x, lineP.start.y, hitVideo.pos.x, hitVideo.pos.y);
-            rect(hitVideo.pos.x, hitVideo.pos.y, hitVideo.size.x, hitVideo.size.y);
-          } else {
-            drawingContext.setLineDash([10, 5]);
-            line(lineP.start.x, lineP.start.y, lineP.end.x, lineP.end.y);
-          }
+          if (!hitVideo) drawingContext.setLineDash([10, 5]);
+          line(lineP.start.x, lineP.start.y, lineP.end.x, lineP.end.y);
           pop();
-          if (ballManager.isUserHost && hitVideo && hitVideo !== ball.target) {
-            ballManager.changeTarget(hitVideo);
+          if (hitVideo && hitVideo !== ball.target) {
+            ballManager.setTarget(hitVideo);
           }
           break;
         }
@@ -115,7 +110,7 @@ function ballThrowed(isHost = false) {
   if (ballManager.ball.from.ID === localVideo.ID) {
     Send(CATCHBALL, { mode: BALLMODE, state: BALLMODE_THROWING });
     ballManager.setMode(BALLMODE_THROWING);
-    if (ballManager.selectMode === catchUserTypes[1]) {
+    if(ballManager.selectMode === catchUserTypes[1]){
       ballManager.isUserHost = false;
     }
   }
@@ -126,9 +121,9 @@ function ballThrowed(isHost = false) {
  */
 function ballArrived(isHost = false) {
   ballManager.setMode(BALLMODE_TRACKING);
-  if (ballManager.selectMode === catchUserTypes[1]) {
-    ballManager.isUserHost = true;
+  if(ballManager.selectMode === catchUserTypes[1]){
     ballManager.setTarget();
+    ballManager.isUserHost = true;
   }
   Send(CATCHBALL, { mode: BALLMODE, state: STATE_CATCH });
   if (isHost) {
@@ -183,7 +178,7 @@ function getPointingLine(video) {
  */
 function getCollVideo(from, pointingLine) {
   if (!pointingLine) return;
-  if (collLineVideo(localVideo, from, pointingLine)) return localVideo;
+  if(collLineVideo(localVideo, from, pointingLine)) return localVideo;
   for (let i = 0; i < others.length; i++) {
     if (collLineVideo(others[i], from, pointingLine)) {
       return others[i];
@@ -249,7 +244,7 @@ function receiveBallStatus(catchballMode) {
     case STATE_NEXTUSER:
       let target = getVideoInst(catchballMode.target);
       if (isCatchBall) {//2回目以降
-        if (target === ballManager.ball.target) {
+        if(target === ballManager.ball.target) {
           ballManager.changeTarget(target);
         } else {
           ballManager.setTarget(target);
@@ -262,7 +257,7 @@ function receiveBallStatus(catchballMode) {
         ballManager.setTarget(target);
       }
       return;
-
+      
     case USERSELECT:
       ballManager.setUserSelectMode(catchballMode.state);
       return;
@@ -291,12 +286,12 @@ class BallManager {
         this.setTarget(this.getNext());
         break;
       case catchUserTypes[1]:
-        Send(CATCHBALL, { from: this.ball.from.ID, target: undefined, mode: STATE_NEXTUSER });
+        Send(CATCHBALL, {from:this.ball.from.ID, target: undefined, mode:STATE_NEXTUSER });
         break;
     }
   }
-  setUserSelectMode(mode) {
-    if (isCatchBall) {
+  setUserSelectMode(mode){
+    if(isCatchBall) {
       $("#catchUserSelect").val(this.selectMode);
       return false;
     }
@@ -307,19 +302,14 @@ class BallManager {
   setTarget(next) {
     this.ball.setTarget(next);
     if (this.isUserHost) {
-      this.sendBallUsers();
+      let ball = this.ball;
+      let msg = { from: ball.from.ID, target: (ball.target ? ball.target.ID : undefined), mode: STATE_NEXTUSER };
+      if (log) console.log(msg);
+      Send(CATCHBALL, msg);
     }
   }
-  changeTarget(target) {
+  changeTarget(target){
     this.ball.changeTarget(target);
-    if (this.isUserHost) {
-      this.sendBallUsers();
-    }
-  }
-  sendBallUsers() {
-    let msg = { from: this.ball.from.ID, target: this.ball.target.ID, mode: STATE_NEXTUSER };
-    if (log) console.log(msg);
-    Send(CATCHBALL, msg);
   }
   setMode(ballMode) {
     this.ballMode = ballMode;
@@ -361,7 +351,7 @@ class Ball extends Obj {
   }
   update() {
     //目標の人を枠取り
-    if (this.target) {
+    if(this.target){
       stroke(0, 255, 0, 255);
       strokeWeight(5);
       noFill();
@@ -378,13 +368,13 @@ class Ball extends Obj {
   setTarget(target) {
     this.amt = 0;
     if (target) {
-      if (this.target) this.from = this.target;
+      if(this.target) this.from = this.target;
       this.target = target;
     } else {
       this.target = undefined;
     }
   }
-  changeTarget(target) {
+  changeTarget(target){
     this.target = target;
   }
   setPosVec(vec) {
