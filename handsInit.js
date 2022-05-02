2bfc35c5765f4d364a120c12eb04461484a39ad4
NekoCan2000 <60868355NekoCan2000@users.noreply.github.com>
2022-05-02 15:29:28 +0900

    handsのオートフォーマット

--- handsInit.js
+++ handsInit.js
@@ -1,5 +1,5 @@
 setTimeout(() => {
-  if(localVideo === null){
+  if (localVideo === null) {
     window.alert('止まってそうならリロードし（ctrl+R）てください');
   }
 }, 10 * 1000);
@@ -11,7 +11,7 @@ const hands = new Hands({
 });
 //https://google.github.io/mediapipe/solutions/hands#configuration-options より
 hands.setOptions({
-  staticImageMode:false,
+  staticImageMode: false,
   /*falseに設定すると、入力画像をビデオストリームとして扱います。
   最初の入力画像で手の検出を試み、検出が成功したらさらに手のランドマークをローカライズします。
   それ以降の画像では、max_num_hands の手がすべて検出され、対応する手のランドマークが特定されると、
@@ -19,12 +19,12 @@ hands.setOptions({
   これは待ち時間を減らすことができ、ビデオフレームを処理するのに最適な方法です。
   true に設定すると、すべての入力画像に対して手の検出が行われ、
   静的でおそらく無関係な画像のバッチ処理に最適です。デフォルトは false。*/
-  
+
   maxNumHands: 2,
   /*検出するハンドの最大数。デフォルトは2。*/
 
   modelComplexity: 0,//負荷軽減のために0
-//https://github.com/google/mediapipe/issues/2181#:~:text=model.setOptions(%7B%0A%20%20%20%20%20%20modelComplexity%3A%201%0A%20%20%20%20%7D)%3B
+  //https://github.com/google/mediapipe/issues/2181#:~:text=model.setOptions(%7B%0A%20%20%20%20%20%20modelComplexity%3A%201%0A%20%20%20%20%7D)%3B
   /*ハンドランドマークモデルの複雑さ。0または1。ランドマーク精度と推論レイテンシは、一般的にモデルの複雑さによって上昇する。デフォルトは1です。 */
 
   minDetectionConfidence: 0.6,
@@ -35,7 +35,7 @@ hands.setOptions({
   その分遅延が大きくなります．static_image_mode が true の場合は無視され，単にすべての画像に対して手指の検出が行われます．
   デフォルトは0.5。*/
 });
-console.log('hands options',hands.g.j);
+console.log('hands options', hands.g.j);
 
 function onResults(results) {
   localVideo.setResults(results);
